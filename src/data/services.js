f3111f6ced2b36cb31960186ddd4e8c03a8319f8
Lapsar Vitaliy <Lapsar.VM@gazprom-neft.ru>
2024-03-05 12:35:56 +0300

    mega update all content

--- src/data/services.js
+++ src/data/services.js
@@ -1,16 +1,19 @@
 const services = [
   {
     image: "/images/icon/rune.jpg",
+    altText: "'Inn Booking' service icon with heartstone",
     name: "Inn Booking",
     text: "We guarantee your safety and privacy during your stay in the inn. Our team of gladiators is always ready to assist you with any questions.",
   },
   {
     image: "/images/icon/flight.jpg",
+    altText: "'Best Flights' service icon with druid's flight form",
     name: "Best Flights",
     text: "Our zeppelins are equipped with everything necessary for a comfortable flight: comfortable seats, a bar with drinks and snacks, as well as panoramic windows",
   },
   {
     image: "/images/icon/gear.jpg",
+    altText: "'Support' service icon with gear",
     name: "Support",
     text: "Don't hesitate to contact us for help! We are always ready to help you and make your adventure more comfortable and interesting.",
   },
--- src/data/tours.js
+++ src/data/tours.js
@@ -1,30 +1,33 @@
 const tours = [
+  
   {
-    image: "",
+    image: "/images/location/tanaris.webp",
     name: "Tanaris",
-    location: "Kalimdor",
-    text: "",
-    price: "",
-    rating: "4.7",
-    slug: ""
+    location: "Southern Kalimdor",
+    text: "Tanaris, or the Tanaris Desert, is a desert located in Southern Kalimdor, east of Un'Goro Crater and south of Thousand Needles. It is an arid land, covered in sand dunes and frequently exposed to dangerous sandstorms.[4] The Steamwheedle Cartel goblins have established Steamwheedle Port and Gadgetzan as their main centers of operation. The dock in Steamwheedle Port was presumably used for transport to the outer islands of the Undermine, the goblin capital, but it was destroyed in the Cataclysm, which also brought the ocean to Gadgetzan, where the goblins built a new dock for their shipping needs.",
+    price: "1250k",
+    rating: "4.2",
+    slug: "tanaris",
   },
+  
   {
-    image: "",
+    image: "/images/tour/nagrand.webp",
     name: "Nagrand",
     location: "Outland",
-    text: "",
-    price: "",
-    rating: "",
-    slug: ""
+    text: "Nagrand is a zone located in Outland. It lies south of Zangarmarsh, west of Terokkar Forest, and southwest of Hellfire Peninsula. It is the last remaining unscarred region of Outland.",
+    price: "4300k",
+    rating: "4.9",
+    slug: "nagrand",
   },
+  
   {
-    image: "",
+    image: "/images/tour/stranglethorn.webp",
     name: "Stranglethorn Vale",
     location: "Eastern Kingdoms",
-    text: "",
-    price: "",
-    rating: "4.9",
-    slug: ""
+    text: "Stranglethorn Vale is a vast jungle south of Duskwood. After the Cataclysm, Stranglethorn Vale was split by a large sinkhole containing a raging whirlpool called The Sundering at its center. The areas of Stranglethorn are now known as Northern Stranglethorn (aka Stranglethorn Jungle) and The Cape of Stranglethorn.",
+    price: "960k",
+    rating: "4.1",
+    slug: "stranglethorn",
   },
   {
     image: "/images/tour/kezan.webp",
@@ -33,7 +36,8 @@ const tours = [
     text: "The Isle of Kezan located in the South Seas is the tropical homeland of the goblins. Their capital of Undermine is located beneath the island, deep in the bowels of Mount Kajaro. Kezan's tropical palms are bisected with miles of roadway, and even the beaches are beginning to be paved over by goblin ambitions.",
     price: "2110k",
     rating: "4.8",
-    slug: "kezan"
+    slug: "kezan",
+    
   },
   {
     image: "/images/tour/vashjir.webp",
@@ -42,18 +46,18 @@ const tours = [
     text: "Vashj'ir is an underwater region located near the Eastern Kingdoms, off the western coast of Khaz Modan. The ruins of the city located in the Shimmering Expanse were the home of Lady Vashj before the Great Sundering. Travellers can swim throughout the zone and use special underwater mounts to move around it.",
     price: "1990k",
     rating: "4.9",
-    slug: "vashjir"
+    slug: "vashjir",
+    available: true,
   },
   {
-    image: "",
+    image: "/images/tour/hyjal.webp",
     name: "Mount Hyjal",
     location: "Kalimdor",
-    text: "",
-    price: "",
+    text: "Mount Hyjal located in Northern Kalimdor. Hyjal is under heavy siege by Ragnaros, who is trying to destroy the World Tree located in the mountain's summit, Nordrassil. Malfurion Stormrage is returning from the Emerald Dream, and commands the armies of Cenarius, known as the Guardians of Hyjal. The zone makes extensive use of the terrain phasing technology.",
+    price: "1500k",
     rating: "4.3",
-    slug: ""
+    slug: "hyjal",
   },
-]
+];
 
 export default tours;
-
